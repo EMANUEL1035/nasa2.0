a6a893fc22b914dfa371889645b107f96d005c9c
EMANUEL1035 <133671987+EMANUEL1035@users.noreply.github.com>
2023-08-30 08:36:12 -0500

    Add files via upload

---
+++ prueba/codigo/codigo_planetas.js
@@ -0,0 +1,151 @@
+let btn_abrir_solar_1 = document.getElementById('btn_abrir_solar_1');
+let hideText_1_1 = document.getElementById('hideText_1_1');
+let btn_abrir_solar_2 = document.getElementById('btn_abrir_solar_2');
+let hideText_1_2 = document.getElementById('hideText_1_2');
+let btn_abrir_solar_3 = document.getElementById('btn_abrir_solar_3');
+let hideText_1_3 = document.getElementById('hideText_1_3');
+let btn_abrir_solar_4 = document.getElementById('btn_abrir_solar_4');
+let hideText_1_4 = document.getElementById('hideText_1_4');
+let btn_abrir_solar_5 = document.getElementById('btn_abrir_solar_5');
+let hideText_1_5 = document.getElementById('hideText_1_5');
+let btn_abrir_solar_6 = document.getElementById('btn_abrir_solar_6');
+let hideText_1_6 = document.getElementById('hideText_1_6');
+let btn_abrir_solar_7 = document.getElementById('btn_abrir_solar_7');
+let hideText_1_7 = document.getElementById('hideText_1_7');
+let btn_abrir_solar_8 = document.getElementById('btn_abrir_solar_8');
+let hideText_1_8 = document.getElementById('hideText_1_8');
+let btn_abrir_solar_9 = document.getElementById('btn_abrir_solar_9');
+let hideText_1_9 = document.getElementById('hideText_1_9');
+let btn_abrir_solar_10 = document.getElementById('btn_abrir_solar_10');
+let hideText_1_10 = document.getElementById('hideText_1_10');
+
+
+btn_abrir_solar_1.addEventListener('click', toggleText);
+
+function toggleText() {
+    hideText_1_1.classList.toggle ('show');
+
+    if(hideText_1_1.classList.contains('show')) {
+        btn_abrir_solar_1.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_1.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_2.addEventListener('click', toggleText_2);
+
+function toggleText_2() {
+    hideText_1_2.classList.toggle ('show');
+
+    if(hideText_1_2.classList.contains('show')) {
+        btn_abrir_solar_2.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_2.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_3.addEventListener('click', toggleText_3);
+
+function toggleText_3() {
+    hideText_1_3.classList.toggle ('show');
+
+    if(hideText_1_3.classList.contains('show')) {
+        btn_abrir_solar_3.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_3.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_4.addEventListener('click', toggleText_4);
+
+function toggleText_4() {
+    hideText_1_4.classList.toggle ('show');
+
+    if(hideText_1_4.classList.contains('show')) {
+        btn_abrir_solar_4.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_4.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_5.addEventListener('click', toggleText_5);
+
+function toggleText_5() {
+    hideText_1_5.classList.toggle ('show');
+
+    if(hideText_1_5.classList.contains('show')) {
+        btn_abrir_solar_5.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_5.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_6.addEventListener('click', toggleText_6);
+
+function toggleText_6() {
+    hideText_1_6.classList.toggle ('show');
+
+    if(hideText_1_6.classList.contains('show')) {
+        btn_abrir_solar_6.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_6.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_7.addEventListener('click', toggleText_7);
+
+function toggleText_7() {
+    hideText_1_7.classList.toggle ('show');
+
+    if(hideText_1_7.classList.contains('show')) {
+        btn_abrir_solar_7.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_7.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_8.addEventListener('click', toggleText_8);
+
+function toggleText_8() {
+    hideText_1_8.classList.toggle ('show');
+
+    if(hideText_1_8.classList.contains('show')) {
+        btn_abrir_solar_8.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_8.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_9.addEventListener('click', toggleText_9);
+
+function toggleText_9() {
+    hideText_1_9.classList.toggle ('show');
+
+    if(hideText_1_9.classList.contains('show')) {
+        btn_abrir_solar_9.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_9.innerHTML = 'Leer Mas'
+    }
+}
+
+btn_abrir_solar_10.addEventListener('click', toggleText_10);
+
+function toggleText_10() {
+    hideText_1_10.classList.toggle ('show');
+
+    if(hideText_1_10.classList.contains('show')) {
+        btn_abrir_solar_10.innerHTML = 'Leer Menos';
+    }
+    else {
+        btn_abrir_solar_10.innerHTML = 'Leer Mas'
+    }
+}
